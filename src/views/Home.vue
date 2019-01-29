7c07864cdffeb0a92529933e02482fb13c2b11fa
Brandon Conway <conwayb@pdx.edu>
2019-01-29 13:58:36 -0800

    Get rid of Hello World component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="home">
-    <HelloWorld :msg="message"/>
     <AolMap></AolMap>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import AolMap from '@/components/AolMap.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld, AolMap
-  },
-  data: () => {
-    return {
-      message: "Welcome to the Atlas of Oregon Lakes new frontend"
-    }
-  }
+  components: { AolMap },
 }
 </script>
