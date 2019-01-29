ff9fc3187a40aa6739688c29c6686899125cf4d2
Brandon Conway <conwayb@pdx.edu>
2019-01-29 12:40:58 -0800

    Add Vue2-leaflet config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import { L } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+
+// Vue2-leaflet: resolves an issue where the markers do not appear
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 
 Vue.config.productionTip = false
 
