4fc208051192b2b72eb237c0ef4bb3223bc68bd4
Brandon Conway <conwayb@pdx.edu>
2019-02-18 17:21:30 -0800

    Add more Lake detail markup
    Add basic markup and styles for lake detail page

--- src/views/Lake.vue
+++ src/views/Lake.vue
@@ -1,28 +1,58 @@
 <template>
-  <div v-if='lake' id="lake-detail">
-    <h2>I'm {{ lake.name }}</h2>
-    <div class='back'>
-      <router-link :to="{ name: 'home' }">
-        Go back
-      </router-link>
-    </div>
-    <p>Here's all my information</p>
-    <p>
-      <strong>Area</span>&nbsp;</strong>{{ lake.area_sq_km}}</span>
-    </p>
-    <p>
-      <strong>Reachcode</strong>&nbsp;<span>{{ lake.reachcode}}</span>
-    </p>
+  <div v-if='lake' class="lake-detail-wrapper">
+    <div class="gutter"></div>
+    <div class="lake-detail">
+      <div class='back'>
+        <router-link :to="{ name: 'home' }">
+          Go back
+        </router-link>
+      </div>
+      <lake-card class="card" :lake="lake"></lake-card>
+      <div class="lake-detail--body">
+        <div class='col-1'>
+          <p>{{ lake.body }}...</p>
+          <p>
+            <strong>Plants data</strong>
+            <p>...</p>
+          </p>
+          <p>
+            <strong>Mussels data</strong>
+            <p>...</p>
+          </p>
+        </div>
+        <div class="col-2">
+          <p class="map-image--wrapper">
+            <img src='/'/>
+            <div class="caption">
+              <a href="#">Lake</a>|<a href="#">Watershed</a>
+            </div>
+          </p>
+          <p>
+            Documents
+            <ul>
+              <li><a href="#">Basin Statistics (pdf)</a></li>
+              <li><a href="#">Atlas of Oregon Lakes Original book page (pdf)</a></li>
+
+            </ul>
+          </p>
+        </div>
+      </div>
+    </div> <!-- end lake-detail -->
+    <div class="gutter"></div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import LakeCard from '@/components/lake/LakeCard';
 
 export default {
   name: 'lake',
   props: {
     slug: String,
+  },
+  components: {
+    LakeCard
   },
   computed: {
     ...mapGetters(['getCurrentLake']),
@@ -51,12 +81,48 @@ export default {
 </script>
 
 <style scoped lang='scss'>
-  #lake-detail {
-    padding: 15px;
+  .lake-detail-wrapper {
+    display: grid;
+    grid-template-columns: 5vw 1fr 5vw;
+    height: 92vh;
   }
+
+  .lake-detail {
+    background: white;
+    padding: 55px;
+  }
+
+  .lake-detail--body {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    padding: 15px 0;
+
+  }
+
   .back {
+    margin: 10px 0;
     a {
       color: #71b5f1;
     }
   }
+
+  .map-image--wrapper {
+    background: WhiteSmoke;
+    margin: auto;
+
+    img {
+      padding: 100px 100px;
+      border-style: none;
+    }
+  }
+
+  .caption {
+    text-align: center;
+
+    a {
+      padding: 5px;
+    }
+  }
+
+
 </style>
