cab9efa3576d1a99734fc84107bfc847bf11ae18
Brandon Conway <conwayb@pdx.edu>
2019-02-26 17:49:11 -0800

    Add temporary icon tab styles

--- src/components/lake/DataTabs.vue
+++ src/components/lake/DataTabs.vue
@@ -3,9 +3,9 @@
     <div class='tabs'>
       <a
         v-for="section in sections" :href="'#'+section.name.toLowerCase()" :key="section.title"
-        v-bind:class="[section.title == currentSectionTitle ? 'active' : '']"
+        v-bind:class="[section.title == currentSectionTitle ? 'active' : '', section.name]"
         @click="setCurrentSection(section.title)">
-        {{ section.title }}
+        <span>{{ section.title }}</span>
       </a>
     </div>
     <keep-alive>
@@ -57,15 +57,50 @@ export default {
 
 <style scoped lang='scss'>
   .tabs {
-    margin-top: 30px;
+    margin-top: 80px;
   }
 
   a {
-    margin-right: 10px;
+    margin-right: 30px;
+    padding-left: 3px;
+    position: relative;
 
     &.active {
       font-weight: bold;
     }
+
+    &.plants::before {
+      content: '\01F33F';
+    }
+
+    &.mussels::before {
+      content: '\1F41A';
+    }
+
+    &.atlas_text {
+      margin-right: 18px;
+    }
+
+    &.atlas_text::before {
+      content: '\1F4D6';
+      padding: 10px 45px;
+    }
+
+  }
+
+  a.active::before {
+    border-top: 5px solid green;
+    padding-bottom: 10px;
+    top: -71px;
+  }
+
+  a::before {
+    font-size: 50px;
+    position: absolute;
+    top: -68px;
+    left: -5px;
+    border: 1px solid grey;
+    padding: 10px;
   }
 
   .data-section {
