7a9a12ddfb06b91699c1d6fc63e6a9b65a9952b3
Brandon Conway <conwayb@pdx.edu>
2019-03-12 18:17:18 -0700

    Use Vuelayers for Openlayers implementation

--- src/components/AolOLMap.vue
+++ src/components/AolOLMap.vue
@@ -1,42 +1,60 @@
 <template>
   <div class='map-container'>
-    <div id="map" class='map'></div>
+    <vl-map @click="checkLakeBounds($event.pixel)"
+      id="map" class="map" ref="map"
+      :load-tiles-while-animating="false"
+      :load-tiles-while-interacting="false">
+      <vl-view :zoom.sync="zoom" :center.sync="center" ></vl-view>
+
+      <vl-layer-tile id="basemap">
+        <vl-source-xyz
+          :url="baseLayerUrl">
+        </vl-source-xyz>
+      </vl-layer-tile>
+
+      <vl-layer-vector-tile  id="publand" ref="publand">
+        <vl-source-vector-tile
+          :url="VectorPubUrl">
+        </vl-source-vector-tile>
+      </vl-layer-vector-tile>
+
+      <vl-layer-vector id='boundaries' ref="lakes_layer" >
+        <vl-source-vector :features="lake_polygons">
+          <vl-style-box>
+            <vl-style-stroke :color="polygon.stroke" :width="polygon.width"/>
+            <vl-style-fill :color="polygon.fill"/>
+          </vl-style-box>
+        </vl-source-vector>
+      </vl-layer-vector>
+
+      <vl-layer-vector id='markers' ref="lakes_markers" >
+        <vl-source-vector :features="lake_markers">
+          <vl-style-box>
+          <vl-style-circle :radius="marker.radius">
+            <vl-style-stroke :color="marker.color" :width="polygon.width"/>
+            <vl-style-fill :color="marker.fillColor"/>
+          </vl-style-circle>
+          </vl-style-box>
+        </vl-source-vector>
+      </vl-layer-vector>
+
+    </vl-map>
   </div>
 </template>
 
 <script>
 
-import { mapActions, mapGetters } from 'vuex';
-import * as proj from 'ol/proj';
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import MVT from 'ol/format/MVT.js';
-import VectorTileLayer from 'ol/layer/VectorTile.js';
-import VectorTileSource from 'ol/source/VectorTile.js';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import TileLayer from 'ol/layer/Tile.js';
-import OSM, {ATTRIBUTION} from 'ol/source/OSM.js';
+import { mapGetters, mapActions } from 'vuex';
 import stylefunction from 'ol-mapbox-style/stylefunction';
-import Feature from 'ol/Feature';
-import Polygon from 'ol/geom/Polygon';
-import Style from 'ol/style/Style';
-import Stroke from 'ol/style/Stroke';
-import Fill from 'ol/style/Fill';
+import * as proj from 'ol/proj';
 
 export default {
   name: 'aol-ol-map',
   data () {
     return {
       zoom: 8,
-      /* Open Street Maps Base Layer */
-      //baseLayerUrl: "https://{a-c}.tile.osm.org/{z}/{x}/{y}.png",
       baseLayerUrl: "https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
-      //baseLayerUrl: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
-      //otherLayerUrl: 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
       VectorPubUrl: "https://tiles.arcgis.com/tiles/6Miy5NqQWjMYTGFY/arcgis/rest/services/Vector_Publands/VectorTileServer/tile/{z}/{y}/{x}.pbf?token=WlTafmvrujX0RXuKfJg3EP-pTf3RVpG-cy_sRsrA1u1l3807JLmogJVzQMvuS0Gw5F3iqyxZ1nYwG-sg6CSZYfsfCHEVjxs8ghQqHIgaw3Qjv_T93x-O0Y4thLto5iacqfN-TfLWYBlBnwXr60RGjan7-Jm3nhcwAlh69L8DDNJlLJU-_WkbVjUtjlE13O1QOHuTfZVizdmBkUjDTfx4q0xPkzNoJZUcpPr0-vhTH0GznGQH64ytxag82P89G0tfbyaVu7gwkUZSqVA7V9TaPMQSCSR-YrDXqyMwjxAD7zg",
-      //center: [44.72925, -121.0411856],
-      //center: [-121.0411856, 44.72925],
       controls: {
         zoom: true,
       },
@@ -47,7 +65,11 @@ export default {
         fillColor: 'blue',
         fillOpacity: 1
       },
-      polygon_color: "yellow",
+      polygon: {
+        stroke: "yellow",
+        fill: "rgba(0,0,0,0.1)",
+        width: 1
+      }
     }
   },
   components: {
@@ -55,67 +77,54 @@ export default {
   computed: {
     ...mapGetters(
         {lakes: 'getLakes', getLakeBySlug: 'getLakeBySlug'}, 'getCurrentLake'),
-    center () {
-      return proj.fromLonLat([-122.60411856, 43.902925], 'EPSG:3857')
+    center: {
+      get () {
+        return proj.fromLonLat([-121.011856, 43.902925], 'EPSG:3857')
+      },
+      set (newValue) {
+        return newValue;
+      }
     },
+    // this feature creation is unnecessary if we use GeoJSON
+    lake_polygons () {
+      return  this.lakes.map((lake) => {
+        return {
+            type: 'Feature',
+            properties: {
+              name: lake.slug,
+              id: lake.reachcode,
+            },
+            geometry: {
+              type: 'Polygon',
+              coordinates: [lake.geom.map((coord) => {
+                  return proj.fromLonLat([coord[1], coord[0]], 'EPSG:3857')
+              })]
+            }
+          }
+      });
+    },
+    lake_markers () {
+      return this.lakes.map((lake) => {
+        return {
+            type: 'Feature',
+            properties: {
+              name: lake.slug,
+              id: lake.reachcode,
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: proj.fromLonLat(
+                [lake.center[1], lake.center[0]], 'EPSG:3857')
+            }
+          }
+      });
+    }
   },
   methods: {
     ...mapActions([
       'fetchLakes', 'setCurrentLake', 'fitBounds',
       'searchLakes', 'setMapObject'
     ]),
-    checkLakeBounds (pixel) {
-      //let coords = proj.toLonLat([lat, lon], 'EPSG:3857')
-      //console.log(coords)
-      console.log(pixel)
-      // see hitTolerance
-      this.map.forEachFeatureAtPixel(pixel, (feature, layer) => {
-          let lake = this.getLakeBySlug(feature.getProperties().name);
-          this.showSideBar(lake);
-          //console.log(`im a layer ${feature.getProperties()}`);
-        }, {
-        layerFilter: (layer_candidate) => {
-          return layer_candidate.getProperties().name == 'lake';
-        }
-      });
-    },
-    drawLakes () {
-      let lake_features = [];
-      let lakeStyle = new Style({
-        fill: new Fill({
-          color: 'rgba(0, 0, 0, 0.1)'
-        }),
-        stroke: new Stroke({
-          color: '#319FD3',
-          width: 1
-        }),
-      });
-      this.lakes.map((lake) => {
-        //console.log(lake)
-        let transformed_geom = lake.geom.map((coord) => {
-            return proj.fromLonLat([coord[1], coord[0]], 'EPSG:3857');
-        });
-        //console.log(transformed_geom)
-        let polygon = new Polygon([transformed_geom]);
-        //console.log(polygon)
-        let lake_feature = new Feature({
-          geometry: polygon,
-          id: lake.reachcode,
-          name: lake.slug
-        });
-        lake_features.push(lake_feature);
-      });
-      //console.log(lake_features)
-      let lakes_layer = new VectorLayer({
-        source: new VectorSource({
-          features: lake_features
-        }),
-        style: lakeStyle,
-        name: 'lake'
-      });
-      this.lakes_layer = lakes_layer;
-      this.map.addLayer(lakes_layer);
-    },
     showSideBar (lake) {
       this.$router.push({name: 'home', query: {'lake': lake.slug}})
       this.setCurrentLake(lake);
@@ -129,64 +138,56 @@ export default {
         this.setCurrentLake(lake);
         this.fitBounds({geom: lake.geom});
       }
-    }
+    },
+    checkLakeBounds (pixel) {
+      // see hitTolerance
+      this.map.forEachFeatureAtPixel(pixel, (feature, layer) => {
+          let lake = this.getLakeBySlug(feature.getProperties().name);
+          this.showSideBar(lake);
+        },
+        {
+          layerFilter: (layer_candidate) => {
+            return layer_candidate.getProperties().id != 'publands';
+        }
+      });
+    },
+
   },
   mounted () {
-    // if this is all going to 'real' javascript
-    // it shoud probably be a separate module
-      let baselayer = new TileLayer({
-        source: new OSM({
-          url: this.baseLayerUrl
-        })
-      });
 
-      let map = new Map({
-        target: 'map',
-        view: new View({
-          center: this.center,
-          zoom: 9.35
-        }),
-        layers: [baselayer,]
-      });
-
-      let vector_layer = new VectorTileLayer({
-        source: new VectorTileSource({
-          format: new MVT(),
-          url: this.VectorPubUrl
-        }),
-        name: 'publand'
-     });
-
-    let style_url = "https://tiles.arcgis.com/tiles/6Miy5NqQWjMYTGFY/arcgis/rest/services/Vector_Publands/VectorTileServer/resources/styles/root.json?f=json&token=X_yghsylypn97rtHbkDQs2jSvCCfJFjy9c21yubfP5lsiH_i09SBK0N3sjots2YJAyLgyHqhYp1OVPd1X30Bu0PRVPEapAF37bl88xVHK9Yya8Vw_10h0AGKVpXpdZwpSBLj3A5zu6ZGfoB6vJyE13zSCqOPxVYzBIpSoc9HHQiUNwlhAs0swj6FlB4oh-uMHe-1F2ELK-ifG-V0veMnwhuNPn-YNA5W-uOkbjH7SffbwT_rEq1xTqvcBrwP2cpxJtSIWkPfYgIZnP8tdkngqqt-ueTydQcnSctPfD1lLV8.";
-
-    fetch(style_url).then((response)=>{
-      return response.json()
-    }).then((style) => {
-      //console.log(style)
-      //console.log(vector_layer);
-      stylefunction(vector_layer, style, 'esri');
+    this.$refs.map.$createPromise.then(() => {
+      this.map = this.$refs.map;
+      this.setMapObject(this.map);
+      // Fetching LAKES
+      if(!this.lakes.length) {
+        this.fetchLakes().then(()=> {
+          this.selectLakeFromUrl();
+        });
+      }
+      else {
+        console.log('I already have the lakes. I will not fetch them again');
+        this.selectLakeFromUrl();
+      }
     });
 
-    map.addLayer(vector_layer);
-    map.on('click', (e) => {
-      this.checkLakeBounds(e.pixel);
-    })
-    this.map = map;
-    this.setMapObject(map);
-    // Fetching LAKES
-    if(!this.lakes.length) {
-      this.fetchLakes().then(()=> {
-        this.selectLakeFromUrl();
-      }).then(()=> {
-        this.drawLakes();
-      });
-    }
-    else {
-      console.log('I already have the lakes. I will not fetch them again');
-      this.drawLakes();
-      this.selectLakeFromUrl();
-    }
-  }
+    // get vector tile layer styles from arcgis online
+    this.$refs.publand.$createPromise.then(() => {
+        let style_url = "https://tiles.arcgis.com/tiles/6Miy5NqQWjMYTGFY/arcgis/rest/services/Vector_Publands/VectorTileServer/resources/styles/root.json?f=json&token=X_yghsylypn97rtHbkDQs2jSvCCfJFjy9c21yubfP5lsiH_i09SBK0N3sjots2YJAyLgyHqhYp1OVPd1X30Bu0PRVPEapAF37bl88xVHK9Yya8Vw_10h0AGKVpXpdZwpSBLj3A5zu6ZGfoB6vJyE13zSCqOPxVYzBIpSoc9HHQiUNwlhAs0swj6FlB4oh-uMHe-1F2ELK-ifG-V0veMnwhuNPn-YNA5W-uOkbjH7SffbwT_rEq1xTqvcBrwP2cpxJtSIWkPfYgIZnP8tdkngqqt-ueTydQcnSctPfD1lLV8.";
+
+        fetch(style_url).then((response)=>{
+          return response.json()
+        }).then((style) => {
+          // HACK: zoom seems to be off-by-one?
+          style.layers.map((layer) => {
+            layer.minzoom -= 1;
+            layer.maxzoom += 1;
+          });
+          stylefunction(this.$refs.publand.$olObject, style, 'esri');
+        });
+    });
+
+  },
+
 }
 </script>
 
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,10 @@
-import { L } from 'vue2-leaflet'
 import * as proj from 'ol/proj';
 import * as olExtent from  'ol/extent';
 
 // this should be moved to a central place
 const LOADING = 'loading';
 const BASE_URL = '//localhost:8080';
-const MAP_CENTER = [44.72925, -121.0411856];
+const MAP_CENTER = [-121.011856, 43.902925];
 const BOUNDS_PADDING = 120;
 
 const actions = {
@@ -32,28 +31,30 @@ const actions = {
     },
 
     fitBounds (context, options) {
+
         let geom = options['geom'];
         let buffer = options['buffer'];
-        const map = context.rootState.map_object;
+
+        const map = context.rootState.map_object.$map;
         if (geom === null) {
-            map.getView().setZoom(9.35)
+            map.getView().setZoom(8)
             setTimeout(() => {
                 map.getView().setCenter(
-                    proj.fromLonLat([-122.60411856, 43.902925], 'EPSG:3857')
+                    proj.fromLonLat(MAP_CENTER, 'EPSG:3857')
                 )
             }, 0);
             return;
         }
-        setTimeout(() => { map.updateSize();}, 2);
+        setTimeout(() => {
+            map.updateSize();}, 2);
         let transformed_geom = geom.map((coord) => {
             return proj.fromLonLat([coord[1], coord[0]], 'EPSG:3857');
         });
         let bounds = olExtent.boundingExtent(transformed_geom)
         let bounds_buffer = buffer !== undefined ? buffer : BOUNDS_PADDING;
         let padding = Array(4).fill(bounds_buffer);
-        console.log(padding)
         let lake_bounds= olExtent.buffer(bounds, bounds_buffer)
-        console.log('viewport: '+ map.getViewport().height)
+        //console.log('viewport: '+ map.getViewport().height)
 
         setTimeout(() => {
             // possibly just keep zoom the same?
