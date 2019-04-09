7fc1c278b96452698688834485d994b55dc6579b
Brennen Florey <bflorey@pdx.edu>
2019-04-09 13:47:54 -0700

    Finalizes filters and improves z-indexing project-wide

--- src/components/map/AolOLMap.vue
+++ src/components/map/AolOLMap.vue
@@ -56,20 +56,11 @@
       <img src="~@/assets/temp_legend.png" />
     </div>
 
-    <div class="map-controls-wrapper">
-      <div class="map-controls--secondary" v-if="show_filters == true">
-        <layer-switcher
-          @feature-layer-change="selectFeatureLayer" @show_filters="toggleFilters">
-        </layer-switcher>
+    <div class="map-buttons-wrapper">
 
-        <filter-control
-          @filter-change="selectLakesFromFilters">
-        </filter-control>
-      </div>
-
-      <div class="map-buttons--secondary">
+      <div class="map-buttons">
         <a role="button" href="#"
-        v-bind:class="['map-button map-button--layers', { selected: show_filters}]" @click="show_filters = !show_filters">
+        v-bind:class="['map-button map-button--layers', { selected: show_filters}]" @click="show_filters = !show_filters; show_legend = false">
           <svg width="20px" height="22px" viewBox="0 0 20 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                 <path d="M9.98888889,18.3777778 L1.8,12.0111111 L0,13.4111111 L10,21.1888889 L20,13.4111111 L18.1888889,12 L9.98888889,18.3777778 L9.98888889,18.3777778 Z M10,15.5555556 L18.1777778,9.18888889 L20,7.77777778 L10,0 L0,7.77777778 L1.81111111,9.18888889 L10,15.5555556 L10,15.5555556 Z" id="Shape" fill="#555555"></path>
@@ -78,7 +69,7 @@
         </a>
 
         <a role="button" href="#" class="map-button map-button--legend"
-v-bind:class="['map-button map-button--legend', { selected: show_legend}]" @click="show_legend = !show_legend">
+v-bind:class="['map-button map-button--legend', { selected: show_legend}]" @click="show_legend = !show_legend; show_filters = false">
           <svg width="3px" height="16px" viewBox="0 0 3 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                 <g id="Group-2" fill="#2B2B2B" fill-rule="nonzero">
@@ -89,7 +80,24 @@ v-bind:class="['map-button map-button--legend', { selected: show_legend}]" @clic
           </svg>
         </a>
       </div>
+
     </div>
+
+
+
+
+    <div class="map-filter-wrapper" v-if="show_filters == true">
+      <layer-switcher
+        @feature-layer-change="selectFeatureLayer" @show_filters="toggleFilters">
+      </layer-switcher>
+
+      <filter-control
+        @filter-change="selectLakesFromFilters">
+      </filter-control>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -350,6 +358,7 @@ export default {
 
     toggleFilters (toggle_filters) {
       this.show_filters = toggle_filters;
+      show_legend = false;
     }
     // end methods
   },
