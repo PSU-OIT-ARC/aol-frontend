d4a946d7d2397ffbb2ea4a25d70abe64efbe289a
Thom Linton <tlinton@pdx.edu>
2019-08-20 17:17:22 -0700

    Adds route navigation and processing for 'Home' view.
    Lake routes should now be properly replayed within this view.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,10 @@
 
 <script>
 // @ is an alias to /src
+import { mapActions, mapGetters } from 'vuex';
+
 import MapContainer from '@/components/map/MapContainer';
 import SideBar from '@/components/SideBar';
-import { mapGetters } from 'vuex';
 
 export default {
   name: 'home',
@@ -19,9 +20,35 @@ export default {
     MapContainer,
     SideBar
   },
+  methods: {
+    ...mapActions(['setCurrentFocus', 'fitBounds']),
+    ...mapGetters(['getLakeByReachcode']),
+    activateLake (lake) {
+      this.setCurrentFocus(lake);
+      this.fitBounds({lake: lake });
+      this.$router.push({name: 'home', query: {'lake': lake.reachcode}});
+    },
+    deactivateLake () {
+      this.setCurrentFocus();
+      this.fitBounds({});
+      this.$router.push({name: 'home', query: {}});
+    },
+  },
   computed: {
     ...mapGetters(['getCurrentFocus']),
+  },
+  watch: {
+    '$route': function (to) {
+      if (to.name == "home" && to.query && Object.keys(to.query).includes("lake")) {
+        let gl = this.getLakeByReachcode();
+        let lake = gl(parseInt(to.query.lake));
+        this.activateLake(lake);
+      } else if (to.name == "home" && to.query && !Object.keys(to.query).length) {
+        this.deactivateLake();
+      }
+    }
   }
+
 }
 </script>
 
