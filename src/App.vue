8670ecc50d5d9757819fd4941880bf67fc9cfa8e
Thom Linton <tlinton@pdx.edu>
2019-09-02 16:11:19 -0700

    Delegates management of waterbody datasets to root application component.

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,37 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 import NavBar from '@/components/NavBar';
 
 export default {
   name: 'app',
-  components: { NavBar }
+  components: { NavBar },
+  methods: {
+    ...mapActions(['markTimestamp', 'fetchLakes']),
+    ...mapGetters(['getTimeElapsed', 'getLakes']),
+  },
+  created () {
+    if (!this.getLakes().length) {
+      this.markTimestamp('waterbody-major');
+      return this.fetchLakes('major').then(() => {
+        let gte = this.getTimeElapsed();
+        console.debug("Loading major waterbody index took " + gte('waterbody-major') + "ms");
+
+        this.markTimestamp('waterbody-minor');
+        this.fetchLakes('minor').then(() => {
+          let gte = this.getTimeElapsed();
+          console.debug("Loading minor waterbody index took " + gte('waterbody-minor') + "ms");
+        });
+      });
+    } else {
+      return new Promise ((resolve) => {
+        console.info('The lake index is already loaded.');
+        resolve();
+      });
+    }
+  }
 }
 </script>
 
