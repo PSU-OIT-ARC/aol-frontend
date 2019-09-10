85465f16b9fbc31aa5f281290e3942a0c7cbd3d7
Brandon Conway <conwayb@pdx.edu>
2019-09-09 17:18:28 -0700

    Add contact info and footer component for flatpages
    Add new component files and add components to FlatPage view
    
    refs: AOLR-54

---
+++ src/components/AolFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>
+      <a href="http://www.clr.pdx.edu/">Center for Lakes &amp; Reservoirs</a> |
+      <a href="http://www.pdx.edu/geography/center-for-spatial-analysis-and-research">Center for Spatial Analysis &amp; Research</a> |
+      <a href="http://www.pdx.edu/cupa/">College of Urban &amp; Public Affairs</a> |
+      <a href="http://www.pdx.edu/arc/">Academic &amp; Research Computing</a> |
+      <a href="http://www.oregon.gov/DEQ/Pages/index.aspx">Oregon Department of Environmental Quality</a>
+    </p>
+
+    &copy; 1985-2019 Atlas of Oregon Lakes
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+  padding: 15px;
+  background-color: #F3F3F3;
+  margin-top: 40px;
+}
+
+</style>
