878fd2a6dc83964830abbb222977a150337f446e
Thom Linton <tlinton@pdx.edu>
2019-10-08 03:43:22 -0700

    Updates exceptional card styling.

--- src/components/OfflineCard.vue
+++ src/components/OfflineCard.vue
@@ -2,17 +2,16 @@
   <div class='offline-container card-container'>
     <div class='image-background'></div>
     <div class="offline-card card">
-      <div></div>
+      <div class="gutter"></div>
       <div class="content">
         <img src="~@/assets/offline_lake.png" />
-
         <div class="copy">
-          <h3>You are currently in <span>offline mode</span></h3>
+          <h3>You are in offline mode</h3>
           <h4>Please try to use the site again when you have network connection.</h4>
           <p>If you believe you have received this message in error, please let us know.</p>
         </div>
       </div>
-      <div></div>
+      <div class="gutter"></div>
     </div>
   </div>
 </template>
--- src/views/404.vue
+++ src/views/404.vue
@@ -2,28 +2,20 @@
   <div class='not-found-container card-container'>
     <div class='image-background'></div>
     <div class="not-found-card card">
-      <div></div>
+      <div class="gutter"v></div>
       <div class="content">
         <img src="~@/assets/offline_lake.png" />
-
         <div class="copy">
           <h3>Sorry! <span>Error #404</span></h3>
           <h4>The page you're looking for could not be found.</h4>
-          <p><router-link :to="{ name: 'home'}">
-            Try going to the home page and searching &rarr;
-          </router-link></p>
+          <p>
+            Try going to the
+            <router-link :to="{ name: 'home'}">home page</router-link>
+            and searching &rarr;
+          </p>
         </div>
       </div>
-      <div></div>
+      <div class="gutter"></div>
     </div>
   </div>
 </template>
-
-<style scoped lang='scss'>
-  .not-found-card {
-    h3 {
-      font-size: 2em;
-    }
-
-  }
-</style>
