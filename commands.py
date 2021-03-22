e6bb260d04b765c9215ad60749882e31ca00a5f1
Thom Linton <tlinton@pdx.edu>
2021-03-22 14:48:31 -0700

    Uses Emcee 1.1 release.

--- commands.py
+++ commands.py
@@ -1,24 +1,15 @@
 from emcee.runner.config import YAMLCommandConfiguration
-from emcee.runner import command, configs, config
+from emcee.runner import configs
 
 from emcee.commands.deploy import deploy
+from emcee.deploy import deployer
 from emcee.deploy.javascript import LocalProcessor, Deployer
 from emcee.backends.aws.deploy import S3RemoteProcessor
 from emcee.backends.aws.infrastructure.commands import *
 
-configs.load('default', 'commands.yml', YAMLCommandConfiguration)
+configs.load(YAMLCommandConfiguration)
 
 
-class AOLFrontendLocalProcessor(LocalProcessor):
-    def get_package_manager(self):
-        return config.yarn.bin
-
-
+@deployer()
 class AOLFrontendDeployer(Deployer):
-    local_processor_cls = AOLFrontendLocalProcessor
     remote_processor_cls = S3RemoteProcessor
-
-
-@command
-def deploy_app(rebuild=True):
-    deploy(deployer_class=AOLFrontendDeployer, rebuild=rebuild)
